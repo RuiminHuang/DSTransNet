c53c6c769d3a76f3bb9442b8981e809633b6f846
huang_ruimin <huang_ruimin@whu.edu.cn>
2025-05-26 23:25:56 +0800

    Update script

--- utils/script.sh
+++ utils/script.sh
@@ -1,5 +1,5 @@
-tensorboard --port=8010 --samples_per_plugin=images=100000 --logdir=./logs/dev
+tensorboard --port=8010 --samples_per_plugin=images=100000 --logdir=./
 
-nohup python train_SC_TransNet.py > output0.log 2>&1 &
+nohup python train.py > output0.log 2>&1 &
 
 kill -9 1511652
