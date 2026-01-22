efdf64ab622c4b4369a64c20187e47c17a7926b7
huang_ruimin <huang_ruimin@whu.edu.cn>
2026-01-22 13:29:42 +0800

    fix some bug

--- train.py
+++ train.py
@@ -183,7 +183,7 @@ class Trainer(object):
 
         if miou > self.best_miou:
             self.best_miou = miou
-            torch.save(self.net.mode.state_dict(), os.path.join(self.log_dir, 'best_miou.pth'))
+            torch.save(self.net.model.state_dict(), os.path.join(self.log_dir, 'best_miou.pth'))
         if nIoU > self.best_nIoU:
             self.best_nIoU = nIoU
             torch.save(self.net.model.state_dict(), os.path.join(self.log_dir, 'best_nIoU.pth'))
